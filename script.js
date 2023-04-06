f3040b4db4f85f2ea6d448818c06f09a00456249
Aaron <awitthohn@yahoo.com>
2023-04-06 15:48:45 -0400

    adjusted script.js to more accurately show whats happening in the console. added logic to console.log info saved by ID in local storage. Changed for loop, took if statement and made it an else if, added if statement ahead of it to check if loadText was null, adjusted strings in other console logs better describe what was happeneing.

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
 $(function () {
   //adds event listener on click for save button
-  $('.saveBtn').on('click', function() {
+  $('.saveBtn').on('click', function () {
     var savedText = $(this).siblings('.description').val();
+    console.log(savedText)
+    //logs text saved in description
     var id = $(this).parent().attr('id');
+    console.log(id)
+    //logs ID of which hour the text above was saved
     localStorage.setItem(id, savedText);
-    //sets parent to ID and siblings to description means the text is the value shown inside description, when clicked it stores the information in text to the appropriate ID
+    var locallySavedText = localStorage.getItem(id);
+    console.log("Saved value for " + id + " is " + locallySavedText);
+    //logs the value of local storage by id
+    //sets parent to ID and siblings to description. This means the savedText is the value shown inside description, when clicked it stores the text in to the appropriate ID
   });
-  
+
   //adds for loop to load previous saved text for all 24 hrs
-  for (var i = 0; i <=24; i++) {
+  for (var i = 0; i <= 24; i++) {
     var keyId = 'hour-' + i;
     var loadText = localStorage.getItem(keyId);
-    console.log(keyId,loadText)
-  //sets parameters of loop for loadText to pull from the correct stored location
-    if (loadText !== "") {
+     //sets parameters of loop for loadText to pull from the correct stored location
+    if (loadText == null) {
+      console.log(keyId + " nothing found local storage")
+    }
+    else if (loadText !== "") {
       $('#' + keyId + ' .description').val(loadText);
-  //checks if text is empty, if it is not empty it loads/sets the text, returning it for the proper ID/description based on the value of loadText
+      console.log(loadText + " found saved in " + keyId)
+      //checks if text is empty, if it is not empty it loads/sets the text, returning it for the proper ID/description based on the value of loadText
     }
   }
 
-var currentTime = dayjs().hour();
-console.log(currentTime)
-//logs the hour of the day in Military Time
 
-var day = $(".time-block")
-console.log(day) 
-//logs the entire day as an object array. specifically showing each div of time (hour - x ) as 1 part of the array
+  var currentTime = dayjs().hour();
+  console.log(currentTime + " current time in military time")
+  //logs the hour of the day in Military Time
 
-day.each(function () {
-  var hourOfDay = $(this).attr("id").split("-")[1];
-  //takes the id of (this) array and splits it into a new array without the (hour -) 
-  //beginning portion of the ID,  assigning it the new value of (hourOfDay)
+  var day = $(".time-block")
+  console.log(day, "logging hours of the day")
+  //logs the entire day as an object array. specifically showing each div of time (hour - x ) as 1 part of the array
 
-  console.log(hourOfDay, currentTime)
-  if (hourOfDay == currentTime){
-    $(this).addClass("present")
-    console.log("present")
-  }
-  //assigns present to class if hourOfDay loosely equals currentTime
-  else if (hourOfDay < currentTime){
-  $(this).addClass("past")
-  console.log("past")
-  }
-  //assigns past to class to hours that are less than currentTime
-  else if (hourOfDay > currentTime){
-    $(this).addClass("future")
-    console.log("future")
-  }
-  //assigns future to class if hours are greater than currentTime
-})
-//compares if the hourOfDay meets the currentTime and if it does assigns it the present class.
+  day.each(function () {
+    var hourOfDay = $(this).attr("id").split("-")[1];
+    //takes the id of (this) array and splits it into a new array without the (hour -) 
+    //beginning portion of the ID,  assigning it the new value of (hourOfDay)
+
+    console.log(hourOfDay, currentTime,  "  checking if hourOfDay = currentTime")
+    if (hourOfDay == currentTime) {
+      $(this).addClass("present")
+      console.log("present class applied, hourOfDay = currentTime ")
+    }
+    //assigns present to class if hourOfDay loosely equals currentTime
+    else if (hourOfDay < currentTime) {
+      $(this).addClass("past")
+      console.log("past class applied, hourOfDay < currentTime")
+    }
+    //assigns past to class to hours that are less than currentTime
+    else if (hourOfDay > currentTime) {
+      $(this).addClass("future")
+      console.log("future class applied, hourOfDay > currentTime")
+    }
+    //assigns future to class if hours are greater than currentTime
+  })
+  //compares if the hourOfDay meets the currentTime and if it does assigns it the present class.
 
   const todayDate = dayjs().format("dddd, MMMM D YYYY");
   $("#currentDay").text(todayDate);
